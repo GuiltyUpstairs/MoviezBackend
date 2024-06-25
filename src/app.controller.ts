7fad4dfeb144b805c73acde3c822e27c91dd8200
bansalharshit838 <127611244+bansalharshit838@users.noreply.github.com>
2024-06-25 12:57:42 +0530

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  authUser(@Body('username') username, @Body('password') password){
+    return this.appService.checkUser(username, password);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,45 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  //bhai teri maa ka bhosda
+
+  users = [
+    {
+      username : 'abc',
+      password: 'iwhfiuw',
+    },
+    {
+      username:'clown',
+      password: 'buppy',
+    }
+  ]
+
+  checkUser(username: string, password: string){
+    const currentUser = this.users.find(p => p.username == username)
+
+    if(currentUser != null){
+      if(password == currentUser.password){
+        //login successful
+        const reply = {
+          status: 200,
+          message: 'success'
+        }
+        return reply
+      }
+      else{
+        //passwosrd is wrong
+        const reply = {
+          status: 401,
+          message: 'Password is wrong'
+        }
+        return reply
+      }
+    }
+    else{
+      return {
+        status: 401,
+        message: "User does not exist"
+      }
+    }
   }
 }
