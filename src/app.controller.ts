da2b137308b1f298db9c579e985f710647a12787
bansalharshit838 <127611244+bansalharshit838@users.noreply.github.com>
2024-06-25 13:14:58 +0530

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
-
+  @Get()
   @Post()
   authUser(@Body('username') username, @Body('password') password){
     return this.appService.checkUser(username, password);
